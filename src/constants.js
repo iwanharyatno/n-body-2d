268cae9ed071b681ce154ef82573d0f8fc733277
iwanharyatno <iwnharry61@gmail.com>
2022-11-20 20:09:56 +0700

    Add bodies count control and speed up the simulation rate

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const Constants = {
   G: 6.6743e-11,
-  massScale: 1/1e15
+  massScale: 1/1e14
 };
 
 export default Constants;
--- src/ui/NBodySimulation.js
+++ src/ui/NBodySimulation.js
@@ -17,14 +17,33 @@ class NBodySimulation extends HTMLElement {
     this.ctx = this.canvas.getContext('2d');
     container.appendChild(this.canvas);
 
+    const controls = document.createElement('div');
+    controls.setAttribute('class', 'flex justify-evenly items-center w-full mt-4');
+    controls.innerHTML = `
+    <button type="button" class="py-2 px-4 rounded border-none bg-blue-500 text-white focus:bg-blue-700 w-1/3" id="replay-button">Run With</button>
+    <input type="number" id="bodies-count" name="Bodies Count" aria-labelledby="replay-button" class="p-2 rounded border focus:outline focus:outline-blue-700 w-1/3" value="100">
+    <span class="w-16">Bodies</span>
+    `;
+    container.appendChild(controls);
+
+    const replayButton = controls.querySelector('#replay-button');
+    const bodiesCountInput = controls.querySelector('#bodies-count');
+
+    replayButton.addEventListener('click', () => this.generateBodies(bodiesCountInput.value));
+    bodiesCountInput.addEventListener('change', () => this.generateBodies(bodiesCountInput.value));
+
     this.appendChild(container);
 
     this.canvasWidth = this.canvas.width;
     this.canvasHeight = this.canvas.height;
 
+    this.generateBodies();
+  }
+
+  generateBodies(count=100) {
     this.bodies = [];
 
-    for(let i=0; i < 300; i++) {
+    for(let i=0; i < count; i++) {
       const body = new Body();
       body.position.x = randomRange(0, this.canvasWidth);
       body.position.y = randomRange(0, this.canvasHeight);
